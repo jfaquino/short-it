c80c3e9d302a4d27028faa5327fbcd1430e65f43
jfaquino <39652504+jfaquino@users.noreply.github.com>
2024-09-24 10:07:24 -0400

    chore: fix typo in description

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
    icons: "/short-it.svg",
    title: "Short It - Simple and fast URL shortener!",
    description:
-      "A (WIP) user-friendly URL shortening service designed for speed and simplicity. Short It allows users to quickly shorten long URLs, making them easier to share and manage.",
+      "An user-friendly URL shortening service designed for speed and simplicity. Short It allows users to quickly shorten long URLs, making them easier to share and manage.",
 };
 
 export default function RootLayout({
