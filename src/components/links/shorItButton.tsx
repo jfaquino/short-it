64bacb3a0dc586b284f95a0d2d8fed27ae88adba
jfaquino <39652504+jfaquino@users.noreply.github.com>
2024-12-19 17:10:04 -0400

    style: add beta flag to short-it logo

--- src/components/links/shorItButton.tsx
+++ src/components/links/shorItButton.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 export default function ShorItButton() {
    return (
-      <Link href="/" className="flex items-center space-x-2">
+      <Link
+         href="/"
+         className="relative flex items-center space-x-2 hover:opacity-80"
+      >
          <Image
             src="/short-it.svg"
             width={36}
@@ -13,6 +16,14 @@ export default function ShorItButton() {
          <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400">
             Short-it
          </span>
+
+         <div className="absolute top-0 left-full rounded-xl bg-gradient-to-br from-purple-400/50 to-pink-400/50 p-0.5">
+            <div className="flex h-full w-full items-center justify-center rounded-xl bg-black/60 px-2">
+               <span className="text-[0.6rem] font-mono tracking-widest text-white">
+                  Beta
+               </span>
+            </div>
+         </div>
       </Link>
    );
 }
