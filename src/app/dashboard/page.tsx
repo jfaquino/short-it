0fae40aedcc758da8b3181e9a4466b86e83edec0
jfaquino <39652504+jfaquino@users.noreply.github.com>
2024-12-31 12:10:08 -0400

    fix: replace Link component in dashboard to a native anchor

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -126,7 +126,7 @@ export default async function Dashboard() {
                                        size="icon"
                                        asChild
                                     >
-                                       <Link
+                                       <a
                                           href={
                                              generateShortUrl(item.shortCode)
                                                 .url
@@ -134,7 +134,7 @@ export default async function Dashboard() {
                                           target="_blank"
                                        >
                                           <ExternalLink className="h-4 w-4" />
-                                       </Link>
+                                       </a>
                                     </Button>
                                     <Button variant="outline" size="icon">
                                        <Copy className="h-4 w-4" />
