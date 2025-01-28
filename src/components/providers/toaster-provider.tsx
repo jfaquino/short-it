e3b3d7786ab76affbb33da42221f5c74eb2e92f1
jfaquino <39652504+jfaquino@users.noreply.github.com>
2025-01-28 19:44:41 -0400

    refactor: delete unused style in toaster

--- src/components/providers/toaster-provider.tsx
+++ src/components/providers/toaster-provider.tsx
@@ -8,14 +8,7 @@ export const ToasterProvider = () => {
    return (
       <Toaster
          position="bottom-right"
-         theme={theme === "dark" ? "dark" : "light"}
-         toastOptions={{
-            classNames: {
-               toast: "font-sans dark:bg-[#171717] bg-[#f3f4f6] text-[#1f2937] dark:text-[#f5f5f5] dark:border-[#262626]",
-               description: "font-mono",
-               closeButton: "dark:bg-[#262626] dark:hover:bg-[#464646]",
-            },
-         }}
+         theme={theme === "light" ? "light" : "dark"}
       />
    );
 };
