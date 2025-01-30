1f1457c6e76de250784c816e51451aad64a4c08f
jfaquino <39652504+jfaquino@users.noreply.github.com>
2025-01-30 14:53:47 -0400

    refactor: simplified theme-toggler button

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -1,58 +1,28 @@
 "use client";
 
-import { Monitor, MoonIcon, Sun } from "lucide-react";
+import { MoonIcon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import {
-   DropdownMenu,
-   DropdownMenuContent,
-   DropdownMenuItem,
-   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ThemeToggler() {
    const { setTheme } = useTheme();
 
+   const toggleTheme = () => {
+      setTheme((oldTheme) => (oldTheme === "dark" ? "light" : "dark"));
+   };
+
    return (
-      <DropdownMenu>
-         <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" aria-label="Toggle theme">
-               <Sun
-                  className="absolute size-5 dark:scale-0"
-                  strokeWidth={1.5}
-               />
-               <MoonIcon
-                  className="absolute size-5 scale-0 dark:scale-100"
-                  strokeWidth={1.5}
-               />
-            </Button>
-         </DropdownMenuTrigger>
-
-         <DropdownMenuContent align="end">
-            <DropdownMenuItem
-               className="flex items-center gap-3"
-               onClick={() => setTheme("light")}
-            >
-               <Sun className="size-6" />
-               Light
-            </DropdownMenuItem>
-
-            <DropdownMenuItem
-               className="flex items-center gap-3"
-               onClick={() => setTheme("dark")}
-            >
-               <MoonIcon className="size-6" />
-               Dark
-            </DropdownMenuItem>
-
-            <DropdownMenuItem
-               className="flex items-center gap-3"
-               onClick={() => setTheme("system")}
-            >
-               <Monitor className="size-6" />
-               System
-            </DropdownMenuItem>
-         </DropdownMenuContent>
-      </DropdownMenu>
+      <Button
+         variant="ghost"
+         size="icon"
+         aria-label="Toggle theme"
+         onClick={toggleTheme}
+      >
+         <Sun className="absolute size-5 dark:scale-0" strokeWidth={1.5} />
+         <MoonIcon
+            className="absolute size-5 scale-0 dark:scale-100"
+            strokeWidth={1.5}
+         />
+      </Button>
    );
 }
